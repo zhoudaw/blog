629a98542bc9c7b272d388ca72303ad0ed1aeb05
zdwzzz <1224515040@qq.com>
2020-12-07 21:55:13 +0800

    00

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const NAV = require('./nav.js')
 const SIDEBAR = require('./sidebar.js')
 module.exports = {
     base:"/blog/",
-	title: '旺仔秘籍',
+	title: 'fy旺仔',
 	themeConfig: {
 		lastUpdated: '更新时间',
 		sidebar: 'auto',
